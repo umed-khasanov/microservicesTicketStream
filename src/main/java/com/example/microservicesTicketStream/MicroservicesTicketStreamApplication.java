7826176d77468050d30eadbf2eb29f61ce2511bd
Umed <humed256@gmail.com>
2026-07-27 12:44:10 +0100

    Initial commit

---
+++ src/main/java/com/example/microservicesTicketStream/MicroservicesTicketStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesTicketStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesTicketStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesTicketStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservicesTicketStream/MicroservicesTicketStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicesTicketStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesTicketStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
